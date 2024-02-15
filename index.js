0ec90ef5e1f4415fddda1dc39691f8097d234122
Sumanthrillex07 <p.21_ug@cse.nits.ac.in>
2024-02-15 12:33:23 +0530

    cors changes

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
-
+const users = {};
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
   socket.on("join", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} join room: ${data}`);
+  });
+  socket.on("identity",(name)=>{
+    users[socket.id] = { name };
+    console.log(name);
+    socket.broadcast.emit("user",name);
+  })
+  socket.on("send", async (data) => {
+    // console.log(data);
+    await socket.to(data.room).emit("receive", data);
   });
   socket.on("disconnect", () => {
     console.log("User left", socket.id);
